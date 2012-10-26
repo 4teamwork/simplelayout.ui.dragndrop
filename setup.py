f48e08d83f5af920eacc7c41f1864c95d74adb38
Jonas Baumann <jone@jone.ch>
2012-10-26 15:26:40 +0200

    Rename readme to .rst.

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ version = '3.0b3.dev0'
 setup(name='simplelayout.ui.dragndrop',
       version=version,
       description="simplelayout drag and drop support",
-      long_description=open("README.txt").read() + "\n" +
+      long_description=open("README.rst").read() + "\n" +
                        open(os.path.join("docs", "HISTORY.txt")).read(),
       # Get more strings from http://www.python.org/pypi?%3Aaction=list_classifiers
       classifiers=[
